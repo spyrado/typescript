94736b4e7d1f4595578fc1d0bf5063932ed3330b
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2022-09-04 19:58:23 -0300

    att

--- dist/the basics/Emitting with Errors/emitting-with-errors.js
+++ dist/the basics/Emitting with Errors/emitting-with-errors.js
@@ -1,5 +1,4 @@
 function greet(person, date) {
     console.log("Hello ".concat(person, ", today is ").concat(date, "!"));
 }
-greet("Brendan", new Date());
 //# sourceMappingURL=emitting-with-errors.js.map
--- src/typescript/the basics/Emitting with Errors/emitting-with-errors.ts
+++ src/typescript/the basics/Emitting with Errors/emitting-with-errors.ts
@@ -8,4 +8,6 @@ function greet(person: any, date: Date) {
 // para isso vá até o seu tsconfig.json e em "compilerOptions" dentro dele sete o "noEmitOnError": para true. -> "noEmitOnError": true,
 // se você está fazendo uma migração do js para o ts, provavelmente não vai querer barrar erros, pois é uma migração e o sistema já está funcionando, então nesse caso
 // o "noEmitOnError": seria false -> "noEmitOnError": false, e só iria ser ativado após toda a migração seja concluída com sucesso.
-greet("Brendan", 'qualquer-coisa');
+
+// =================== DESCOMENTE AQUI PARA ENTENDER A EXPLICAÇÃO ====================
+// greet("Brendan", 'qualquer-coisa');
